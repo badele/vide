883bcb45629b17f0a2e7f47e930e4c3b60ff64d2
Bruno Adelé <brunoadele@gmail.com>
2023-10-12 12:37:34 +0200

    feat: update edgy configuration, remove some neotree conf

--- lua/plugins/edgy.lua
+++ lua/plugins/edgy.lua
@@ -25,7 +25,7 @@ return {
 				end,
 			},
 			"Trouble",
-			{ ft = "qf", title = "QuickFix" },
+			{ ft = "qf",            title = "QuickFix" },
 			{
 				ft = "help",
 				size = { height = 20 },
@@ -46,24 +46,24 @@ return {
 				end,
 				size = { height = 0.5 },
 			},
-			{
-				title = "Neo-Tree Git",
-				ft = "neo-tree",
-				filter = function(buf)
-					return vim.b[buf].neo_tree_source == "git_status"
-				end,
-				pinned = true,
-				open = "Neotree position=right git_status",
-			},
-			{
-				title = "Neo-Tree Buffers",
-				ft = "neo-tree",
-				filter = function(buf)
-					return vim.b[buf].neo_tree_source == "buffers"
-				end,
-				pinned = true,
-				open = "Neotree position=top buffers",
-			},
+			-- {
+			-- 	title = "Neo-Tree Git",
+			-- 	ft = "neo-tree",
+			-- 	filter = function(buf)
+			-- 		return vim.b[buf].neo_tree_source == "git_status"
+			-- 	end,
+			-- 	pinned = true,
+			-- 	open = "Neotree position=right git_status",
+			-- },
+			-- {
+			-- 	title = "Neo-Tree Buffers",
+			-- 	ft = "neo-tree",
+			-- 	filter = function(buf)
+			-- 		return vim.b[buf].neo_tree_source == "buffers"
+			-- 	end,
+			-- 	pinned = true,
+			-- 	open = "Neotree position=top buffers",
+			-- },
 			--any other neo-tree windows
 			"neo-tree",
 		},
