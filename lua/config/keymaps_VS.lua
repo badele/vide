fd83cb2560d2e9be48056a5c8b7d451490decd9b
Bruno Adelé <brunoadele@gmail.com>
2023-10-14 20:17:45 +0200

    feat: hide the writing file message

--- lua/config/keymaps_VS.lua
+++ lua/config/keymaps_VS.lua
@@ -4,6 +4,5 @@ end
 
 -- Visual Studio keys mapping
 -- map({ "i", "v", "n", "s" }, "<C-n>", "<cmd>tabnew<cr><esc>", { desc = "New file (VS)" })
-map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file (VS)" })
-map({ "i", "v", "n", "s" }, "<C-p>", "<cmd>Telescope oldfiles<cr>", {desc = "Recently opened files (VS)" })
-
+map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>silent w<cr><esc>", { desc = "Save file (VS)" })
+map({ "i", "v", "n", "s" }, "<C-p>", "<cmd>Telescope oldfiles<cr>", { desc = "Recently opened files (VS)" })
