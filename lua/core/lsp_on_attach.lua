2a768e1c407f5189dba016c59a187c70e7f9df68
Bruno Adelé <brunoadele@gmail.com>
2023-10-16 13:13:33 +0200

    feat: update hover documentation and code signature

--- lua/core/lsp_on_attach.lua
+++ lua/core/lsp_on_attach.lua
@@ -8,18 +8,26 @@ local function on_attach(_, bufnr)
 
   local opts = { buffer = bufnr }
 
-  map({ "n" }, "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", opts)
+  -- Hover & signature
   map({ "n" }, "<leader>ch", vim.lsp.buf.hover, "[C]ode [H]over", opts)
+  map({ "n" }, "<leader>cs", vim.lsp.buf.signature_help, "[C]ode [S]ignature", opts)
+  map({ "n" }, "K", vim.lsp.buf.hover, "[C]ode [H]over", opts)
+  map({ "n" }, "<C-k>", vim.lsp.buf.signature_help, "[C]ode [S]ignature", opts)
+
+  -- Format
   map({ "n" }, "<leader>cf", function()
     vim.lsp.buf.format { async = true }
   end, "[C]ode [F]ormat", opts)
   map({ "n" }, "<leader>cF", "<cmd>Format<cr>", "Another [C]ode [F]ormater", opts)
+
+  -- Some actions & code navigation
+  map({ "n" }, "<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", opts)
   map({ "n" }, "<leader>cr", vim.lsp.buf.rename, "[C]ode [R]ename", opts)
   map({ "n" }, "gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration", opts)
   map({ "n" }, "gd", vim.lsp.buf.definition, "[G]oto [D]efinition", opts)
   map({ "n" }, "gi", vim.lsp.buf.implementation, "[G]oto [I]mplementation", opts)
   map({ "n" }, "gr", vim.lsp.buf.references, "[G]oto [R]eference", opts)
-  map({ "n" }, "<C-k", vim.lsp.buf.signature_help, "[C]ode signature", opts)
+  map({ "n" }, "<C-k>", vim.lsp.buf.signature_help, "[C]ode signature", opts)
   map({ "n" }, '<leader>wa', vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd", opts)
   map({ "n" }, '<leader>wr', vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove", opts)
   map({ "n" }, '<leader>wl', function()
