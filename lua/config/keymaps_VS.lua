e07cc8535da5d4768db96f5d2cd9128c566e259f
Bruno Adelé <brunoadele@gmail.com>
2023-11-20 19:18:55 +0100

    feat: update code diagnostic key binding

--- lua/config/keymaps_VS.lua
+++ lua/config/keymaps_VS.lua
@@ -3,9 +3,14 @@ local function map(mode, lhs, rhs, opts)
 end
 
 -- Visual Studio keys mapping
--- map({ "i", "v", "n", "s" }, "<C-n>", "<cmd>tabnew<cr><esc>", { desc = "New file (VS)" })
+map({ "i", "v", "n", "s" }, "<C-n>", "<cmd>tabnew<cr><esc>", { desc = "New file (VS)" })
 map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>silent w<cr><esc>", { desc = "Save file (VS)" })
 map({ "i", "v", "n", "s" }, "<C-p>", "<cmd>Telescope oldfiles<cr>", { desc = "Recently opened files (VS)" })
+
+-- Global mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+-- shift+arrow selection
+map({ "n", "i" }, "<F8>", vim.diagnostic.goto_next, { desc = "Go to next problem" })
 
 -- shift+arrow selection
 map({ "n" }, "<S-Up>", "V", { desc = "Arrow Up selection" })
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -94,13 +94,6 @@ return {
       end
     }
 
-    -- Global mappings.
-    -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-    vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-    vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
-    vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-    vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
-
     -- Use LspAttach autocommand to only map the following keys
     -- after the language server attaches to the current buffer
     vim.api.nvim_create_autocmd('LspAttach', {
