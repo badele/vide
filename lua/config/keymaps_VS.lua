106c28333096a8b6e8616101c42232881585f4a8
Bruno Adelé <brunoadele@gmail.com>
2023-11-26 12:20:04 +0100

    feat: register for change mode

--- lua/config/keymaps_VS.lua
+++ lua/config/keymaps_VS.lua
@@ -2,15 +2,20 @@ local function map(mode, lhs, rhs, opts)
 	vim.keymap.set(mode, lhs, rhs, opts)
 end
 
+-------------------------------------------------------------------------------
+-- Visual Studio Code
+-------------------------------------------------------------------------------
+
 -- Visual Studio keys mapping
 map({ "i", "v", "n", "s" }, "<C-n>", "<cmd>tabnew<cr><esc>", { desc = "New file (VS)" })
 map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>silent w<cr><esc>", { desc = "Save file (VS)" })
 map({ "i", "v", "n", "s" }, "<C-p>", "<cmd>Telescope oldfiles<cr>", { desc = "Recently opened files (VS)" })
 
--- Global mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
--- shift+arrow selection
-map({ "n", "i" }, "<F8>", vim.diagnostic.goto_next, { desc = "Go to next problem" })
+
+-- Error or Warning navigation
+map({ "n", "i" }, "<F8>", vim.diagnostic.goto_next, { desc = "Go to next problem (VS)" })
+map({ "n", "i" }, "<F20>", vim.diagnostic.goto_prev, { desc = "Go to prev problem (VS)" })
+
 
 -- shift+arrow selection
 map({ "n" }, "<S-Up>", "V", { desc = "Arrow Up selection" })
@@ -19,3 +24,13 @@ map({ "i" }, "<S-Up>", "<ESC>V", { desc = "Arrow Up selection" })
 map({ "i" }, "<S-Down>", "<ESC>V", { desc = "Arrow Down selection" })
 map({ "v" }, "<S-Up>", "k", { desc = "Arrow Up selection" })
 map({ "v" }, "<S-Down>", "j", { desc = "Arrow Down selection" })
+
+-------------------------------------------------------------------------------
+-- MISC
+-------------------------------------------------------------------------------
+
+-- Save all buffers
+map({ "i", "v", "n", "s" }, "<C-S-S>", "<cmd>silent wa<cr><esc>", { desc = "Save all files", remap = false })
+
+-- Not copy to register during change
+map({ "n", "i" }, 'c', '"_c', { desc = "Remap change" })
