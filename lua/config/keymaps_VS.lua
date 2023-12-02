bfaea2a784c008e1b0ce509431dce7b14b894f37
Bruno Adelé <brunoadele@gmail.com>
2023-12-02 05:40:22 +0100

    feat: add search & replace on file

--- lua/config/keymaps_VS.lua
+++ lua/config/keymaps_VS.lua
@@ -9,13 +9,28 @@ end
 -- Visual Studio keys mapping
 map({ "i", "v", "n", "s" }, "<C-n>", "<cmd>tabnew<cr><esc>", { desc = "New file (VS)" })
 map({ "i", "v", "n", "s" }, "<C-s>", "<cmd>silent w<cr><esc>", { desc = "Save file (VS)" })
+map({ "i", "v", "n", "s" }, "<C-S-S>", "<cmd>silent wa<cr><esc>", { desc = "Save all files", remap = false })
 map({ "i", "v", "n", "s" }, "<C-p>", "<cmd>Telescope oldfiles<cr>", { desc = "Recently opened files (VS)" })
 
+-- Search
+map({ "i" }, "<C-f>", '<esc>/', { desc = "Search (VS)" })
+map({ "v", "n", "s" }, "<C-f>", '/', { desc = "Search (VS)" })
+
+-- Search & Replace
+map({ "n" }, "<C-S-h>", '<esc><cmd>lua require("spectre").open_visual(select_word=true)<CR>',
+	{ desc = "Replace files (VS)" })
+map({ "v" }, "<C-S-h>", '<esc><cmd>lua require("spectre").open_visual()<CR>',
+	{ desc = "Replace files (VS)" })
+
+-- Search & Replace on current file
+map({ "n",  }, "<C-h>", '<esc><cmd>lua require("spectre").open_file_search(select_word=true)<CR>',
+	{ desc = "Replace on current file (VS)" })
+map({ "v",  }, "<C-h>", '<esc><cmd>lua require("spectre").open_file_search()<CR>',
+	{ desc = "Replace on current file (VS)" })
 
 -- Error or Warning navigation
 map({ "n", "i" }, "<F8>", vim.diagnostic.goto_next, { desc = "Go to next problem (VS)" })
 map({ "n", "i" }, "<F20>", vim.diagnostic.goto_prev, { desc = "Go to prev problem (VS)" })
-
 
 -- shift+arrow selection
 map({ "n" }, "<S-Up>", "V", { desc = "Arrow Up selection" })
@@ -28,9 +43,6 @@ map({ "v" }, "<S-Down>", "j", { desc = "Arrow Down selection" })
 -------------------------------------------------------------------------------
 -- MISC
 -------------------------------------------------------------------------------
-
--- Save all buffers
-map({ "i", "v", "n", "s" }, "<C-S-S>", "<cmd>silent wa<cr><esc>", { desc = "Save all files", remap = false })
 
 -- Not copy to register during change
 map({ "n" }, 'c', '"_c', { desc = "Remap change" })
