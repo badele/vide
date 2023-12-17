1c53059117b35792ab8e1bd35f0fdc899b72ca52
Bruno Adelé <brunoadele@gmail.com>
2023-12-17 18:27:30 +0100

    fix: Remove color background on enter edit mode, because, break hilight

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -21,14 +21,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 	end
 })
 
-vim.api.nvim_create_autocmd("InsertEnter", {
-	callback = function()
-		vim.api.nvim_set_hl(0, "Normal", { bg = "#202232" })
-	end
-})
-
-vim.api.nvim_create_autocmd("InsertLeave", {
-	callback = function()
-		vim.api.nvim_set_hl(0, "Normal", { bg = "#222436" })
-	end
-})
+-- vim.api.nvim_create_autocmd("InsertEnter", {
+-- 	callback = function()
+-- 		vim.api.nvim_set_hl(0, "Normal", { bg = "#202232" })
+-- 	end
+-- })
+--
+-- vim.api.nvim_create_autocmd("InsertLeave", {
+-- 	callback = function()
+-- 		vim.api.nvim_set_hl(0, "Normal", { bg = "#222436" })
+-- 	end
+-- })
