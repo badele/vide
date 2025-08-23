fdea9fa44fa666aef72ad7da63da47f95ed16199
Bruno Adelé <brunoadele@gmail.com>
2025-08-23 10:35:52 +0000

    fix: treesister markview dependency

--- lua/plugins/code/treesitter.lua
+++ lua/plugins/code/treesitter.lua
@@ -1,6 +1,7 @@
 -- Nvim Treesitter configurations and abstraction layer
 return {
 	"nvim-treesitter/nvim-treesitter",
+	dependencies = { "OXY2DEV/markview.nvim" },
 	event = { "BufReadPost", "BufNewFile" },
 	build = ":TSUpdate",
 	opts = {
