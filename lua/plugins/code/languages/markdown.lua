969dd20dfcae468855bb10c84eb0fbdaca7b6c3e
Bruno Adelé <brunoadele@gmail.com>
2025-10-15 19:43:15 +0000

    feat: disable markview addons

--- lua/plugins/code/languages/markdown.lua
+++ lua/plugins/code/languages/markdown.lua
@@ -36,13 +36,13 @@ return {
 		},
 	},
 
-	{
-		"OXY2DEV/markview.nvim",
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-tree/nvim-web-devicons"
-		},
-	},
+	-- {
+	-- 	"OXY2DEV/markview.nvim",
+	-- 	dependencies = {
+	-- 		"nvim-treesitter/nvim-treesitter",
+	-- 		"nvim-tree/nvim-web-devicons"
+	-- 	},
+	-- },
 
 	{
 		"iamcco/markdown-preview.nvim",
